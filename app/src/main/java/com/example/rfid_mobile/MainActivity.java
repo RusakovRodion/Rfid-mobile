355a4dc52b83ce8a100d6c4352f12e171c12ca45
Alice <vlasovaru931@gmail.com>
2022-03-27 22:22:41 +0300

    Попытка создать каталог [ не работает :( ]

--- app/src/main/java/com/example/rfid_mobile/MainActivity.java
+++ app/src/main/java/com/example/rfid_mobile/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.rfid_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<ObjectClass> objects = new ArrayList<ObjectClass>();
+        if(objects.size()==0){
+            objects.add(new ObjectClass(0, "Очки", "Прекрасные  очки", true, "Очки"));
+            objects.add(new ObjectClass(1, "Очки", "Волшебные  очки", true, "Очки"));
+            objects.add(new ObjectClass(2, "Очки", "Великолепные  очки", true, "Очки"));
+        }
+
+        // получаем элемент ListView
+        ListView objectsList = findViewById(R.id.objectsList);
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, objects);
+        // устанавливаем для списка адаптер
+        objectsList.setAdapter(adapter);
+        // добавляем для списка слушатель
     }
 }
---
+++ app/src/main/java/com/example/rfid_mobile/ObjectClass.java
@@ -0,0 +1,17 @@
+package com.example.rfid_mobile;
+
+public class ObjectClass {
+    public int id;
+    public String name;
+    public String description;
+    public boolean status;
+    public String category;
+
+    ObjectClass(int id, String name, String description, boolean status, String category){
+        this.name = name;
+        this.id=id;
+        this.description=description;
+        this.status=status;
+        this.category=category;
+    }
+}
