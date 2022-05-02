d10c8daa397d934367e18bf81fafc73d9a7761d0
osence <tri29092000@mail.ru>
2022-05-02 20:26:34 +0300

    Add deleteObject and editObject buttons and alertMessages

--- app/src/main/java/com/example/rfid_mobile/ObjectActivity.java
+++ app/src/main/java/com/example/rfid_mobile/ObjectActivity.java
@@ -1,14 +1,25 @@
 package com.example.rfid_mobile;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ObjectActivity extends AppCompatActivity {
 
     ObjectClass object;
-
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +39,50 @@ public class ObjectActivity extends AppCompatActivity {
         } else {
             status.setText("Статус: арендован");
         }
+
+        Button editObjectButton = findViewById(R.id.editObjectButton);
+        Button deleteObjectButton = findViewById(R.id.deleteObjectButton);
+        builder = new AlertDialog.Builder(this);
+        editObjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setTitle("Внимание")
+                        .setMessage("Вы точно хотите редактировать "+name.getText()+"?")
+                        .setCancelable(true)
+                        .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        }).setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        })
+                        .show();
+            }
+        });
+        deleteObjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setTitle("Внимание")
+                        .setMessage("Вы точно хотите удалить "+name.getText() +"?")
+                        .setCancelable(true)
+                        .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        }).setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                })
+                        .show();
+            }
+        });
     }
 }
+
